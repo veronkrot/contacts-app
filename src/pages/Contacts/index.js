0bcc5f48b4e574cdfcbe14dff23980252615976a
veronkrot <veronkrotenko9@gmail.com>
2020-11-26 18:04:35 +0200

    added contacts table

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,15 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useContacts } from "../../hooks/useContact";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import { useStyles } from "./style";
+import { ContactsTable } from "./ContactsTable";
 
 export const Contacts = () => {
+  const classes = useStyles();
   const contacts = useContacts();
 
-  if (contacts.isLoading) {
-    return <div>...loading</div>;
-  }
+  return (
+    <Container className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} className={classes.headContainer}>
+          <Typography variant="h4" component="h1">
+            Contacts{" "}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {(() => {
+            if (contacts.isLoading) {
+              return <div>...loading</div>;
+            }
 
-  if (contacts.isError) {
-    return <div>...error</div>;
-  }
-  return <div>Contacts {contacts.data[0].name.first}</div>;
+            if (contacts.isError) {
+              return <div>...error</div>;
+            }
+            return <ContactsTable data={contacts.data} />;
+          })()}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
