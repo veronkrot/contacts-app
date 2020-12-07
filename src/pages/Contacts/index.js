80fbafb9aab3e487eb7437684053a769a4056fdd
veronkrot <veronkrotenko9@gmail.com>
2020-12-07 17:07:44 +0200

    added toggle data view mode

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -3,21 +3,31 @@ import { useContacts } from "../../hooks/useContact";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { useStyles } from "./style";
 import { ContactsTable } from "./ContactsTable";
+import { ToggleDataViewMode } from "./ToggleDataViewMode/index";
+import { DATA_VEW_MODES } from "./constants";
+import { useDataViewMode } from "../../hooks/useDataViewMode";
 
 export const Contacts = () => {
   const classes = useStyles();
   const contacts = useContacts();
-
+  const [dataViewMode, setDataViewMode] = useDataViewMode();
   return (
     <Container className={classes.root}>
       <Grid container>
         <Grid item xs={12} className={classes.headContainer}>
-          <Typography variant="h4" component="h1">
-            Contacts{" "}
-          </Typography>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="h4" component="h1">
+              Contacts
+            </Typography>
+            <ToggleDataViewMode
+              dataViewMode={dataViewMode}
+              setDataViewMode={setDataViewMode}
+            />
+          </Box>
         </Grid>
         <Grid item xs={12}>
           {(() => {
@@ -28,7 +38,13 @@ export const Contacts = () => {
             if (contacts.isError) {
               return <div>...error</div>;
             }
-            return <ContactsTable data={contacts.data} />;
+            if (dataViewMode === DATA_VEW_MODES.TABLE) {
+              return <ContactsTable data={contacts.data} />;
+            }
+
+            if (dataViewMode === DATA_VEW_MODES.GRID) {
+              return "grid";
+            }
           })()}
         </Grid>
       </Grid>
